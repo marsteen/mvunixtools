5c2e7142a3aaf1e22783c0d1e57ec523f665c8a0
Martin Steen <martin@martin-steen.de>
2024-08-24 17:04:52 +0200

    added path to project_source.sh

--- projectheader/project_source.sh
+++ projectheader/project_source.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if ["$1" == ""]
 then
-echo "usage:   project_source <keyfile.txt>" 
-echo "example: project_source keyfile.txt"
+echo "usage:   project_source <keyfile.txt> <path>" 
+echo "example: project_source keyfile.txt ."
 else
-find . \( -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) -exec projectheader $1 {} \;
+find $2 \( -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) -exec projectheader $1 {} \;
 fi
 
